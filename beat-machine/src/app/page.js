97ef494070f23d043b2c76e968b41e42bd169211
Shon Abdullah <Abdullah@Skeno-Abdu-Rahman.local>
2024-06-28 23:33:22 -0500

    moving on to fcc testing. mobile styling sufficient. added a disclaimer: the app isnt appropriately responsive on mobile, but it is visible

--- beat-machine/src/app/page.js
+++ beat-machine/src/app/page.js
@@ -6,6 +6,12 @@ export default function Home() {
   return (
     <div>
       <BeatMachine />
+      <br />
+      <h1
+        id="disappearing"
+        className="text-center -mt-10">
+        Not very functional on Mobile lol. Please visit again from a desktop!
+      </h1>
     </div>
   );
 }
